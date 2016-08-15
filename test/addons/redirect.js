f89438ae964e4481de5a5d27b8f65d9c11558ac0
Max Frigge <max.frigge@gmail.com>
2016-08-15 17:29:26 +0200

    feat(addon): add redirect() addon

---
+++ test/addons/redirect.js
@@ -0,0 +1,37 @@
+import test from 'tape'
+import {
+  setupKoa,
+  teardownKoa
+} from '../utils/koa'
+import middleware from '../../src/core/middleware'
+import redirect from '../../src/addons/redirect'
+
+test('Addon: redirect()', (t) => {
+  t.plan(2)
+
+  const koa = setupKoa()
+  const task = [
+    redirect('/redirect-target')
+  ]
+  koa.app.use(
+    middleware({
+      task
+    })
+  )
+
+  koa.request.get('/').end((err, res) => {
+    if (!err) {
+      t.equal(
+        res.status,
+        302,
+        'should set http status code 302'
+      )
+      t.equal(
+        res.header.location,
+        '/redirect-target',
+        'should set the given path as location header'
+      )
+    }
+    teardownKoa(koa)
+  })
+})
