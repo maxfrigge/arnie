7cc63d274f594b5b33b449ae47020323904a085f
Max Frigge <max.frigge@gmail.com>
2016-08-27 19:46:21 +0200

    fix: remove named exports (not working)

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 // Just for ease of use when importing
-// module.exports = require('./dist').default
-
-module.exports = {
-  default: require('./dist').default,
-  createTask: require('./dist').createTask,
-  runTask: require('./dist').runTask,
-  runTasks: require('./dist').runTasks
-}
+module.exports = require('./dist').default
