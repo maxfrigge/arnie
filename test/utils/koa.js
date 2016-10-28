da9efe2c6f6f699158643770b0c74fd4e8073960
Max Frigge <max.frigge@gmail.com>
2016-10-28 18:33:35 +0200

    chore: add missing deps

--- test/utils/koa.js
+++ test/utils/koa.js
@@ -1,18 +1,18 @@
-const Koa = require('koa')
-const supertest = require('supertest')
-
-module.exports.setupKoa = () => {
-  const app = new Koa()
-  const server = app.listen()
-  const request = supertest.agent(server)
-
-  return {
-    app,
-    request,
-    server
-  }
-}
-
-module.exports.teardownKoa = (koa) => {
-  koa.server.close()
-}
+// const Koa = require('koa')
+// const supertest = require('supertest')
+//
+// module.exports.setupKoa = () => {
+//   const app = new Koa()
+//   const server = app.listen()
+//   const request = supertest.agent(server)
+//
+//   return {
+//     app,
+//     request,
+//     server
+//   }
+// }
+//
+// module.exports.teardownKoa = (koa) => {
+//   koa.server.close()
+// }
