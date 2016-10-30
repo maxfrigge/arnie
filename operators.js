388b2123bce83e9890d70e9bec4520af227cf1cd
Max Frigge <max.frigge@gmail.com>
2016-10-30 23:15:24 +0100

    feat: use es5 syntax in import helper

--- operators.js
+++ operators.js
@@ -1,7 +1,5 @@
 // Just for ease of use when importing
-const set = require('./lib/operators/set')
-const when = require('./lib/operators/when')
 module.exports = {
-  set,
-  when
+  set: require('./lib/operators/set'),
+  when: require('./lib/operators/when')
 }
